e28a6b57091ad17daec748a27918d5622188564f
ekawutsuw <ekawut.suw@outlook.com>
2017-01-04 23:11:05 +0000

    added text for top-chart

--- graphs_js/travel.js
+++ graphs_js/travel.js
@@ -84,7 +84,7 @@ function setUpDataEar() {
     // Earnings in the UK by month (NSA)
     var raw_nsay2014 = [1405,1187,1320,1526,1791,2121,2669,2568,2178,1790,1571,1725],
         raw_nsay2015 = [1346,1043,1405,1604,2019,2195,2429,2473,2239,2028,1490,1802],
-        raw_nsay2016 = [1305,1100,1306,1688,1958,2123,2530,2570,2110,0,0,0];
+        raw_nsay2016 = [1305,1100,1306,1688,1958,2123,2530,2570,2110,1860,0,0];
     var nsay2014 = [],
         nsay2015 = [],
         nsay2016 = []
@@ -93,7 +93,7 @@ function setUpDataEar() {
     for (var i = 0; i < 12; i++) {
         nsay2014.push({x: i, y: raw_nsay2014[i]});
         nsay2015.push({x: i, y: raw_nsay2015[i]});
-        if(i<9) nsay2016.push({x: i, y: raw_nsay2016[i]});
+        if(i<10) nsay2016.push({x: i, y: raw_nsay2016[i]});
     }
 
     return [
@@ -119,7 +119,7 @@ function setUpDataExp() {
     // Expenditure abroad by month (NSA)
     var raw_nsay2014 = [2323,2118,2281,2727,3053,3414,3543,4983,4150,3248,2019,1677],
         raw_nsay2015 = [2371,2071,2548,2901,3255,3690,4442,5506,4461,3445,2365,1972],
-        raw_nsay2016 = [2448,2523,2607,3529,3489,3969,4550,6070,5200,0,0,0];
+        raw_nsay2016 = [2448,2523,2607,3529,3489,3969,4550,6070,5200,4030,0,0];
     var nsay2014 = [],
         nsay2015 = [],
         nsay2016 = []
@@ -128,7 +128,7 @@ function setUpDataExp() {
     for (var i = 0; i < 12; i++) {
         nsay2014.push({x: i, y: raw_nsay2014[i]});
         nsay2015.push({x: i, y: raw_nsay2015[i]});
-        if(i<9) nsay2016.push({x: i, y: raw_nsay2016[i]});
+        if(i<10) nsay2016.push({x: i, y: raw_nsay2016[i]});
     }
 
     return [
